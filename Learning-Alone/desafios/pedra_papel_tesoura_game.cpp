583f74a699f74e20e4402b659279f3311c893436
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-05-06 13:54:03 -0300

    melhorei o codigo

--- Learning-Alone/desafios/pedra_papel_tesoura_game.cpp
+++ Learning-Alone/desafios/pedra_papel_tesoura_game.cpp
@@ -1,41 +1,58 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-// chamei as funções de GetUserChoice, GetComputerChoice, ShowChoice e ChosseWinner
-// para organizar o código e facilitar a leitura, mas não são obrigatórias
+/*
+
+?   Esse é um jogo simples de Pedra, Papel e Tesoura entre um jogador e o computador.
+?   O jogador escolhe entre pedra, papel ou tesoura, e o computador faz uma escolha aleatória.
+?   O vencedor é determinado com base nas regras do jogo:
+?   - Pedra vence Tesoura
+?   - Tesoura vence Papel
+?   - Papel vence Pedra
+?   - Se ambos escolherem a mesma opção, é um empate
+
+
+*/
+
+//* chamei as funções de GetUserChoice, GetComputerChoice, ShowChoice e ChosseWinner
 
 char GetUserChoice();
 char GetComputerChoice();
 void ShowChoice(char choice);
 void ChosseWinner(char player, char computer);
 
-
-main(){
+main()
+{
 
     char player;
     char computer;
-
 
     player = GetUserChoice();
     cout << "Escolha do jogador: " << player << endl;
     ShowChoice(player);
 
+    cout << "Escolha do computador: " << endl;
+    computer = GetComputerChoice();
+    ShowChoice(computer);
 
+    ChosseWinner(player, computer);
+    //TODO Chama a função ChosseWinner para determinar o vencedor com base nas escolhas do jogador e do computador
+    //TODO A função imprime o resultado do jogo, indicando se houve empate ou quem venceu
 }
 
-
 /*
-criei as funções GetUserChoice, GetComputerChoice, ShowChoice e ChosseWinner
+* criei as funções GetUserChoice, GetComputerChoice, ShowChoice e ChosseWinner
 */
 
+//* GetUserChoice: função para obter a escolha do jogador
 
-// GetUserChoice: função para obter a escolha do jogador
-
-char GetUserChoice(){
+char GetUserChoice()
+{
 
     char player;
     cout << "jogo de Pedra-Papel-Tesoura" << endl;
-    
+
     do
     {
         cout << "Escolha entre 1 das opcoes: " << endl;
@@ -43,40 +60,113 @@ char GetUserChoice(){
         cout << "digite 'p' para escolher pedra" << endl;
         cout << "digite 'a' para escolher papel " << endl;
         cout << "digite 't' para escolher tesoura" << endl;
+        cout << "digite 's' para sair do jogo" << endl;
+        cout << "****************************" << endl;
         cin >> player;
-        cout << player;
-        
-    }while(player != 'p' && player != 'a' && player != 't' );
+        cout << "****************************" << endl;
+
+    } while (player != 'p' && player != 'a' && player != 't' && player != 's'); //TODO Enquanto o jogador não escolher uma opção válida, continua pedindo a entrada
+    if (player == 's') //TODO Se o jogador escolher 's', o jogo termina
+    {
+        cout << "Saindo do jogo..." << endl;
+        exit(0); //TODO Encerra o programa
+    }
+    cout << "****************************" << endl;
 
     return player;
-    
+
+    // A função retorna a escolha do jogador, que deve ser 'p', 'a' ou 't'
+    // Se o jogador não digitar uma dessas opções, a função continua pedindo a entrada
 }
 
 // GetComputerChoice: função para obter a escolha do computador
-char GetComputerChoice(){
-    
+char GetComputerChoice()
+{
+
+    srand(time(0)); // Inicializa o gerador de números aleatórios com o tempo atual
+    // Gera um número aleatório entre 1 e 3
+    int num = rand() % 3 + 1;
+
+    switch (num) // A função retorna a escolha do computador com base no número gerado
+    // 1 = pedra, 2 = tesoura, 3 = papel
+    {
+    case 1:
+        return 'p';
+    case 2:
+        return 't';
+    case 3:
+        return 'a';
+    }
 }
 
 // ShowChoice: função para mostrar a escolha do jogador ou computador
 // A função recebe um caractere como argumento e imprime a escolha correspondente
-void ShowChoice(char choice) {
-    switch (choice) {
-        case 'p': cout << "Pedra" << endl; break;
-        case 'a': cout << "Papel" << endl; break;
-        case 't': cout << "Tesoura" << endl; break;
+void ShowChoice(char choice)
+{
+    switch (choice)
+    {
+    case 'p':
+        cout << "Pedra" << endl;
+        break;
+    case 'a':
+        cout << "Papel" << endl;
+        break;
+    case 't':
+        cout << "Tesoura" << endl;
+        break;
     }
 }
 
 // ChosseWinner: função para determinar o vencedor do jogo
 // A função recebe as escolhas do jogador e do computador como argumentos e imprime o resultado
-void ChosseWinner(char player, char computer){
-    if (player == computer) {
-        cout << "Empate!" << endl;
-    } else if ((player == 'p' && computer == 't') ||
-               (player == 'a' && computer == 'p') ||
-               (player == 't' && computer == 'a')) {
-        cout << "Vencedor: Jogador!" << endl;
-    } else {
-        cout << "Vencedor: Computador!" << endl;
+void ChosseWinner(char player, char computer)
+{
+    switch (player)
+    {
+    case 'p':
+        if (computer == 'p')
+        {
+            cout << "Empate!" << endl;
+        }
+        else if (computer == 't')
+        {
+            cout << "Jogador venceu!" << endl;
+        }
+        else if (computer == 'a')
+        {
+            cout << "Computador venceu!" << endl;
+        }
+        break;
+    case 'a':
+        if (computer == 'a')
+        {
+            cout << "Empate!" << endl;
+        }
+        else if (computer == 'p')
+        {
+            cout << "Jogador venceu!" << endl;
+        }
+        else if (computer == 't')
+        {
+            cout << "Computador venceu!" << endl;
+        }
+        break;
+    case 't':
+        if (computer == 't')
+        {
+            cout << "Empate!" << endl;
+        }
+        else if (computer == 'a')
+        {
+            cout << "Jogador venceu!" << endl;
+        }
+        else if (computer == 'p')
+        {
+            cout << "Computador venceu!" << endl;
+        }
+        break;
+    default:
+        cout << "Escolha inválida!" << endl;
+        break; // Caso o jogador faça uma escolha inválida, imprime uma mensagem de erro
     }
 }
